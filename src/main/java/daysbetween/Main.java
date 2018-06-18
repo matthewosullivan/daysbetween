7dd8b5d8b2a452b859044713458d9e64932ef419
matthewosullivan <matthewosullivan@michaelpage.com.au>
2018-06-18 17:43:36 +1000

    initial

---
+++ src/main/java/daysbetween/Main.java
@@ -0,0 +1,17 @@
+package daysbetween;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String start = "01/01/1901";
+		String end = "31/12/2999";
+
+		// incrementor
+		
+		// try to increment day, keep incrementing given month, year keep count of days
+		
+		// leap year calcuator
+		
+	}
+
+}
